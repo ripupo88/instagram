ac005ca7477f409865ef1086e1370e4a0baf5fc7
Richar <rpupo88@gmail.com>
2020-01-19 02:31:39 +0000

    2

--- example.js
+++ example.js
@@ -2,7 +2,7 @@ require("./src/tools-for-instagram.js");
 let fs = require("fs");
 let request = require("request");
 
-(async () => {
+let mifun = async () => {
   console.log("\n1 -- LOGIN --\n".bold.underline);
   let instaURL = "https://www.instagram.com/p/";
   let ig = await login();
@@ -10,35 +10,40 @@ let request = require("request");
   let feedSaved = await ig.feed.saved();
   var mySaved = await feedSaved.items();
 
-  do {
-    console.log("\n2 -- Get Last img on saved List -- \n".bold.underline);
+  if (mySaved.length > 0) {
+    do {
+      console.log("\n2 -- Get Last img on saved List -- \n".bold.underline);
 
-    let pic = mySaved[mySaved.length - 1];
-    await sleep(2);
-    await sleep(3);
-    let imgId = pic.pk;
-    let code = pic.code;
-    let pk = pic.user.pk;
-    let userName = "@" + pic.user.username;
-    let caption = `model ${userName} Pomoted by @hot_models_world_2020`;
-    await sleep(8);
-    let url = await getPhotoUrl(ig, instaURL + code);
-    await sleep(5);
-    download(url, "img/img.jpg", async function() {
-      console.log("bajada");
+      let pic = mySaved[mySaved.length - 1];
+      await sleep(2);
+      await sleep(3);
+      let imgId = pic.pk;
+      let code = pic.code;
+      let pk = pic.user.pk;
+      let userName = "@" + pic.user.username;
+      let caption = `model ${userName} Pomoted by @hot_models_world_2020`;
+      await sleep(8);
+      let url = await getPhotoUrl(ig, instaURL + code);
       await sleep(5);
-      await uploadPicture(ig, caption, "img/img.jpg", pk, info.pk);
-      await sleep(20);
-      fs.unlink("img/img.jpg", function() {
-        console.log("deleted");
+      download(url, "img/img.jpg", async function() {
+        console.log("bajada");
+        await sleep(5);
+        await uploadPicture(ig, caption, "img/img.jpg", pk, info.pk);
+        await sleep(20);
+        fs.unlink("img/img.jpg", function() {
+          console.log("deleted");
+        });
+        await ig.media.unsave(imgId);
       });
-      await ig.media.unsave(imgId);
-    });
-    let timeSleep = Math.ceil(Math.random() * 3600 * 7) + 10800;
-    console.log(timeSleep);
-    await sleep(timeSleep);
-  } while (mySaved.length > 0);
-})();
+      let timeSleep = Math.ceil(Math.random() * 3600 * 7) + 10800;
+      console.log(timeSleep);
+      await sleep(timeSleep);
+    } while (mySaved.length > 0);
+  } else {
+    await sleep(300);
+    mifun();
+  }
+};
 
 let download = (uri, filename, callback) => {
   request.head(uri, function(err, res, body) {
@@ -51,3 +56,5 @@ let download = (uri, filename, callback) => {
       .on("close", callback);
   });
 };
+
+mifun();
